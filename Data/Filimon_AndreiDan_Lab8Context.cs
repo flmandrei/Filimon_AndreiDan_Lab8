d35159ebc0ddf99f538596bd28699511fcb45177
Andrei Filimon <andrei.filimon@evozon.com>
2021-01-05 21:17:09 +0200

    first commit

---
+++ Data/Filimon_AndreiDan_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Filimon_AndreiDan_Lab8.Models;
+
+namespace Filimon_AndreiDan_Lab8.Data
+{
+    public class Filimon_AndreiDan_Lab8Context : DbContext
+    {
+        public Filimon_AndreiDan_Lab8Context (DbContextOptions<Filimon_AndreiDan_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Filimon_AndreiDan_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Filimon_AndreiDan_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Filimon_AndreiDan_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Details.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Filimon_AndreiDan_Lab8.Data;
+using Filimon_AndreiDan_Lab8.Models;
+
+namespace Filimon_AndreiDan_Lab8.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Filimon_AndreiDan_Lab8.Data.Filimon_AndreiDan_Lab8Context _context;
+
+        public DetailsModel(Filimon_AndreiDan_Lab8.Data.Filimon_AndreiDan_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Book
+                .Include(b=>b.Publisher)
+                .Include(b=>b.BookCategories)
+                .ThenInclude(b=>b.Category)
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
